8c542e06d0e6a7ccb0dab6d5d7ba829caca621ae
Hamzakhan2317 <73998902+Hamzakhan2317@users.noreply.github.com>
2023-01-31 02:52:19 -0800

    updates

--- src/client.js
+++ src/client.js
@@ -2,11 +2,12 @@ import SanityClient from "@sanity/client";
 import ImageUrlBuilder from "@sanity/image-url";
 
 const client = SanityClient({
-  projectId:0kwf58de,
+  projectId: "0kwf58de",
   dataset: "production",
   apiversion: "2022-02-01",
   useCdn: true,
-  token:'skS81hI8WUsOEbHFPjr9X3wlltGoalw5F1szloI1rzBKCbIYbnc5tbJFhxfsZC5QLqY04sRx2mck9mpid5OE8nI1PZTovEmnzNGOk0mtJZ0EaSFDKYt5hdpCLJvDMLyYYI59oXzkrd7t8glTNoZff2z6r5i5JSjqfHPwfPJcKtwcS6KZAKtp',
+  token:
+    "skS81hI8WUsOEbHFPjr9X3wlltGoalw5F1szloI1rzBKCbIYbnc5tbJFhxfsZC5QLqY04sRx2mck9mpid5OE8nI1PZTovEmnzNGOk0mtJZ0EaSFDKYt5hdpCLJvDMLyYYI59oXzkrd7t8glTNoZff2z6r5i5JSjqfHPwfPJcKtwcS6KZAKtp",
 });
 
 const builder = ImageUrlBuilder(client);
