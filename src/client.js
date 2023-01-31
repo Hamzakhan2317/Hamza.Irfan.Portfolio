9972bfb4e3a6de6eb0563a6ac4af3d4921d875b4
Hamzakhan2317 <73998902+Hamzakhan2317@users.noreply.github.com>
2023-01-31 02:49:38 -0800

    env removed

--- src/client.js
+++ src/client.js
@@ -6,7 +6,7 @@ const client = SanityClient({
   dataset: "production",
   apiversion: "2022-02-01",
   useCdn: true,
-  token:skS81hI8WUsOEbHFPjr9X3wlltGoalw5F1szloI1rzBKCbIYbnc5tbJFhxfsZC5QLqY04sRx2mck9mpid5OE8nI1PZTovEmnzNGOk0mtJZ0EaSFDKYt5hdpCLJvDMLyYYI59oXzkrd7t8glTNoZff2z6r5i5JSjqfHPwfPJcKtwcS6KZAKtp,
+  token:'skS81hI8WUsOEbHFPjr9X3wlltGoalw5F1szloI1rzBKCbIYbnc5tbJFhxfsZC5QLqY04sRx2mck9mpid5OE8nI1PZTovEmnzNGOk0mtJZ0EaSFDKYt5hdpCLJvDMLyYYI59oXzkrd7t8glTNoZff2z6r5i5JSjqfHPwfPJcKtwcS6KZAKtp',
 });
 
 const builder = ImageUrlBuilder(client);
